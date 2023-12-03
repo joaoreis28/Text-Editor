533a1682283a6185263d5638f872eec58bf45eec
joaoreis28 <reisjoaomendes@gmail.com>
2023-12-03 20:42:44 -0300

    Adding new flags.

--- kilo.c
+++ kilo.c
@@ -22,7 +22,9 @@ void enableRawMode()
     atexit(disableRawMode);
     struct termios raw = orig_termios;
 
-    raw.c_iflag &= ~(ICRNL | IXON);
+    raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+    raw.c_oflag &= ~(OPOST);
+    raw.c_cflag |= (CS8);
     raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
 
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
@@ -40,11 +42,11 @@ int main()
     {
         if(iscntrl(c))
         {
-            printf("%d\n", c);
+            printf("%d\r\n", c);
         }
         else
         {
-            printf("%d ('%c')\n", c, c);
+            printf("%d ('%c')\r\n", c, c);
         }
     }
     
